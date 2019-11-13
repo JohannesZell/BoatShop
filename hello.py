63e247bb363e3a72f3574c8641fb7d5b76576979
Johannes Zell <johannes@gifzly.de>
2019-11-13 22:10:23 +0100

    Added JSON parser functionality

--- hello.py
+++ hello.py
@@ -1,14 +1,29 @@
 from flask import Flask, send_from_directory
 import os
+import json
 app = Flask(__name__)
 
 port = int(os.getenv('PORT', 8000))
+boat = {}
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
 
+@app.route('/getBoats')
+def getBoats():
+    jsonBoat = open('JSONobjects.json')
+    jsonBoatStr = jsonBoat.read()
+    boatData = json.loads(jsonBoatStr)
+    return boatData
+
 if __name__ == '__main__':
-    #app.run()
+    # jsonBoat = open('JSONobjects.json')
+    # jsonBoatStr = jsonBoat.read()
+    # boatData = json.loads(jsonBoatStr)
+    # for key in boatData:
+    #     print(key["id"])
+    #     id = key["id"]
+    #     for value in key.items():
+    #         print(boatData[id-1]["name"])
     app.run(host='0.0.0.0', port=port, debug=True)
-    print( "Hello World")
